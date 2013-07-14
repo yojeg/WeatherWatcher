9c376131706430cdc9ec6a7e8cc56f0c12ae7b05
ulrihiv <igoru86@mail.ru>
2013-07-14 21:13:52 +0600

    code cleanup

---
+++ WeatherMonitor.Web/Models/WeatherModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WeatherMonitor.Web.Models
+{
+    using System;
+
+    public class WeatherModel
+    {
+        public int Preassure { get; set; }
+        public int Degrees { get; set; }
+        public decimal WindSpeed { get; set; }
+        public decimal Humidity { get; set; }
+        public DateTime WeatherTime { get; set; }
+    }
+}
--- WeatherSaverConsole/Program.cs
+++ WeatherSaverConsole/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace WeatherMonitor.WeatherSaverConsole
 {
+    using DataAccessLayer;
+    using Domain.Entities;
+
     class Program
     {
         static void Main(string[] args)
